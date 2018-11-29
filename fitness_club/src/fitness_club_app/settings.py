534ae74516448e7e92c5f315fa64bc1e55ac8c82
Michał Wiśniewski <mikex929@gmail.com>
2018-11-29 21:12:59 +0100

    Django admin static files added to project

--- fitness_club/src/fitness_club_app/settings.py
+++ fitness_club/src/fitness_club_app/settings.py
@@ -104,7 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 
 # ADD global ENV to production environment
